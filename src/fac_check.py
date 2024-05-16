b31f06eb75deda71dd0d3f6b37328190797727ec
ek2992 <36238899+ek2992@users.noreply.github.com>
2024-05-16 12:40:29 -0700

    change file names

---
+++ src/fac_check.py
@@ -0,0 +1,9 @@
+import numpy as np
+import fib2
+
+a = np.ones(8,'d')
+
+fib2.fib(a)
+print(a,'here')
+am= fib2.fib(5)
+print(am)
---
+++ src/fib.py
@@ -0,0 +1,6 @@
+import numpy as np
+import fib1
+
+a =np.zeros(8,"d")
+fib1.fib(a)
+print(a)
